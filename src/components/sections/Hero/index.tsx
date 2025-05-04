90a384bd3425f05b21dd044d9ac34e785ad0ba91
lenakang <kangnara92@gmail.com>
2025-05-05 00:35:47 +0900

    first commit

---
+++ src/components/sections/Hero/index.tsx
@@ -0,0 +1,20 @@
+import Image from "next/image";
+import styles from "./styles.module.scss";
+
+export default function index() {
+    return (
+        <section className={styles.hero}>
+            <div className={styles.hero__box}>
+                <Image src="/hero_1.jpg" alt="Couple" fill priority />
+            </div>
+            <Image
+                className={styles.hero__logo}
+                src="/logo_dark.png"
+                alt="logo"
+                width={1024}
+                height={1024}
+                priority
+            />
+        </section>
+    );
+}
---
+++ src/utils/Typewriter/Typewriter.tsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from "react";
+import "./style.css";
+
+const Typewriter = ({
+    words = ["wedding", "lovely", "soft, strong, forever"],
+    typingSpeed = 200,
+    displayTime = 2000,
+    pauseTime = 500,
+    loop = true,
+}) => {
+    const [currentWordIndex, setCurrentWordIndex] = useState(0);
+    const [displayedText, setDisplayedText] = useState("");
+    const [isTyping, setIsTyping] = useState(true);
+
+    useEffect(() => {
+        let timeout: NodeJS.Timeout;
+
+        if (isTyping) {
+            const currentWord = words[currentWordIndex];
+            if (displayedText.length < currentWord.length) {
+                timeout = setTimeout(() => {
+                    setDisplayedText(
+                        currentWord.slice(0, displayedText.length + 1)
+                    );
+                }, typingSpeed);
+            } else {
+                timeout = setTimeout(() => {
+                    setIsTyping(false);
+                }, displayTime);
+            }
+        } else {
+            timeout = setTimeout(() => {
+                setDisplayedText("");
+                setCurrentWordIndex((prev) =>
+                    prev + 1 < words.length ? prev + 1 : loop ? 0 : prev
+                );
+                setIsTyping(true);
+            }, pauseTime);
+        }
+
+        return () => clearTimeout(timeout);
+    }, [
+        displayedText,
+        isTyping,
+        currentWordIndex,
+        words,
+        typingSpeed,
+        displayTime,
+        pauseTime,
+        loop,
+    ]);
+
+    return (
+        <span className="typewriter">
+            {displayedText}
+            <span className="blinking-cursor">|</span>
+        </span>
+    );
+};
+
+export default Typewriter;
