4934b40b9a7d5306ff3a3735cd17fdc7e6a745b3
lenakang <kangnara92@gmail.com>
2025-05-16 11:20:30 +0900

    fix : metadata

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import "aos/dist/aos.css";
 import "./page.scss";
 import AOS from "aos";
 import { useEffect } from "react";
+import { Metadata } from "next";
 import {
     About,
     Invite,
@@ -17,6 +18,25 @@ import {
     Thanks,
 } from "@/components/sections";
 import Script from "next/script";
+
+export const metadata: Metadata = {
+    title: "나래❤️대승 결혼합니다.",
+    description: "6월 28일 (토) 오전 11시 / 더컨벤션 신사",
+    openGraph: {
+        title: "나래❤️대승 결혼합니다.",
+        description: "6월 28일 (토) 오전 11시 / 더컨벤션 신사",
+        url: "https://wedding-iota-seven.vercel.app/",
+        type: "website",
+        images: [
+            {
+                url: "https://wedding-iota-seven.vercel.app/og_img.jpg",
+                width: 800,
+                height: 400,
+                alt: "나래와 대승의 결혼식 초대",
+            },
+        ],
+    },
+};
 
 export default function Home() {
     useEffect(() => {
