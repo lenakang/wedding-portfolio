5f7e3fa26db22ad395335cdc87717575f9707d4b
lenakang <kangnara92@gmail.com>
2025-05-16 14:44:53 +0900

    test : kakao navi

--- src/components/sections/Map/DestinationNavigator.tsx
+++ src/components/sections/Map/DestinationNavigator.tsx
@@ -12,6 +12,23 @@ import {
     NavigatorProps,
 } from "@/utils/navigationHandlers";
 
+declare global {
+    interface Window {
+        Kakao: {
+            isInitialized: () => boolean;
+            init: (key: string) => void;
+            Navi: {
+                start: (config: {
+                    name: string;
+                    x: number;
+                    y: number;
+                    coordType: "wgs84" | "katec";
+                }) => void;
+            };
+        };
+    }
+}
+
 export default function DestinationNavigator({
     lat,
     lng,
