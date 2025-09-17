8e2fe8f90c037dc1784768d432b222628e3c8a3c
lenakang <kangnara92@gmail.com>
2025-09-17 21:51:18 +0900

    fix: 배포 URL 수정

--- src/constants/info.ts
+++ src/constants/info.ts
@@ -19,5 +19,5 @@ export const WEDDING_VENUE = "그랜드 하얏트 서울";
 export const WEDDING_HALL = "그랜드볼룸홀";
 export const WEDDING_DESCRIPTION = `${WEDDING_DATE} / ${WEDDING_VENUE}`;
 
-export const SITE_URL = "wedding-iota-seven.vercel.app";
+export const SITE_URL = "wedding-portfolio-brown.vercel.app";
 export const OG_IMAGE_URL = `${SITE_URL}/og_img.jpg`;
